b5494e04bb38304c406ca3e31d8bae9ff5c2d96b
Pahrijal Saban Mubarok <kevinsudrajat27@gmail.com>
2024-08-15 15:16:14 +0700

    first commit

---
+++ src/main/java/pahrijal_saban_mubarok/restful/TesMsibPtSeiApplication.java
@@ -0,0 +1,13 @@
+package pahrijal_saban_mubarok.restful;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TesMsibPtSeiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TesMsibPtSeiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pahrijal_saban_mubarok/restful/TesMsibPtSeiApplicationTests.java
@@ -0,0 +1,13 @@
+package pahrijal_saban_mubarok.restful;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TesMsibPtSeiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

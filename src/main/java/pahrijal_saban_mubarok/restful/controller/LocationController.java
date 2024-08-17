b4e74507c9c4dcd5e8fe9b8333df52ae3be22985
Pahrijal Saban Mubarok <kevinsudrajat27@gmail.com>
2024-08-17 20:11:31 +0700

    fix typo

--- src/main/java/pahrijal_saban_mubarok/restful/controller/LocationController.java
+++ src/main/java/pahrijal_saban_mubarok/restful/controller/LocationController.java
@@ -24,7 +24,7 @@ public class LocationController {
         locationService.addLocation(request);
         return WebResponse.<String>builder()
                 .status("success")
-                .message("Lokasi Berhasil Ditambahkan")
+                .message("lokasi berhasil ditambahkan")
                 .build();
     }
 
